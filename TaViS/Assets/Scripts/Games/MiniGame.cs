a920b2a69325e310b571c1b5f2750c38eaf60d87
doingitraith <raith.jakob@outlook.com>
2019-06-04 20:44:50 +0200

    MiniGame base class and MiniGame Manager

---
+++ TaViS/Assets/Scripts/Games/MiniGame.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class MiniGame
+{
+    GameID.GAME_ID Id;
+    public string GameName;
+    public Transform CameraStartPosition;
+    public Quaternion CameraStartRotation;
+
+    public abstract void OnGameStarted();
+    public abstract void OnGameFinished();
+    public abstract void OnGameFailed();
+    public abstract void ResetGame();
+}
--- TaViS/Assets/Scripts/Managers/MiniGameManager.cs
+++ TaViS/Assets/Scripts/Managers/MiniGameManager.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class MiniGameManager : MonoBehaviour
 {
+    private MiniGame currMiniGame;
+
+    public Dictionary<GameID.GAME_ID, MiniGame> miniGames;
+    
     // Start is called before the first frame update
     void Start()
     {
-<<<<<<< HEAD
         gestureManager = GameObject.Find("GameManager").GetComponent<GestureManager>();
         gestureManager.LoadGameGestures(GameManager.currentGame);
-=======
-        
->>>>>>> parent of 50858ad... the real push from 31.5
+        InitMiniGames();
+        currMiniGame = miniGames[GameManager.currentGame];
     }
 
     // Update is called once per frame
@@ -21,9 +23,15 @@ public class MiniGameManager : MonoBehaviour
         
     }
 
+    private void InitMiniGames()
+    {
+        miniGames = new Dictionary<GameID.GAME_ID, MiniGame>();
+        // TODO: Add Mini Game classes
+        // miniGames.Add(GameID.GAME_ID.DANCE, FindObjectOfType<DanceMiniGame>());
+    }
+
     public void TriggerGestureResult(GestureEvaluationResult gestureEvaluationResult)
     {
-<<<<<<< HEAD
         //handle score, trigger event with name...
         int score = gestureEvaluationResult.score;
     }
@@ -32,10 +40,8 @@ public class MiniGameManager : MonoBehaviour
     {
         //do stuff
         gestureManager.LoadGameGestures(GameManager.currentGame);
+        currMiniGame = GameManager.currentGame;
     }
-
-=======
         // do stuff
     }
->>>>>>> parent of 50858ad... the real push from 31.5
 }
