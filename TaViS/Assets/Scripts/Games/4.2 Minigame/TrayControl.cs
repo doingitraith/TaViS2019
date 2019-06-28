c9852366c43aed755ec2ef4f84c9bddce21e93ff
ga45fiy <sakuraanae@gmail.com>
2019-06-28 16:19:04 +0200

    added balk and knob

--- TaViS/Assets/Scripts/Games/4.2 Minigame/TrayControl.cs
+++ TaViS/Assets/Scripts/Games/4.2 Minigame/TrayControl.cs
@@ -1,18 +1,69 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Windows.Kinect;
 
-public class TrayControl : MonoBehaviour
+public class TrayControl : MiniGame
 {
+    GameObject pointMan;
+    Rigidbody tray;
+
+    Vector3 handLeft;
+    Vector3 handRight;
+
+    bool hasGrabbed = false;
+
+    float deltaY;
+
+    public override void OnGameFailed()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void OnGameFinished()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void OnGameStarted()
+    {
+        pointMan = GameObject.Find("KinectModelRel");
+
+        Vector3 handLeft =pointMan.GetComponent<KinectPointManAvatarModel>().getRawWorldPosition(JointType.HandLeft);
+        Vector3 handRight = pointMan.GetComponent<KinectPointManAvatarModel>().getRawWorldPosition(JointType.HandRight);
+    }
+
+    public override void ResetGame()
+    {
+        throw new System.NotImplementedException();
+    }
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        tray = GetComponent<Rigidbody>(); 
+     
+
+        //If player has grabbed handles left and right
+        if((handLeft.position == trayLeftHandle.position) && (handRight.position == trayRightHandle.position))
+        {
+            hasGrabbed = true;
+        } 
+           
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (hasGrabbed)
+        {
+            //berechne deltaY
+        }
         
+
     }
+
+
 }
