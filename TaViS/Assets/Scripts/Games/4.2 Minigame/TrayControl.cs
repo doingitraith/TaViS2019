8791dee976c4cfe79617579caa11aed44f173d74
ga45fiy <sakuraanae@gmail.com>
2019-07-03 23:37:29 +0200

    added sliders

--- TaViS/Assets/Scripts/Games/4.2 Minigame/TrayControl.cs
+++ TaViS/Assets/Scripts/Games/4.2 Minigame/TrayControl.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using Windows.Kinect;
 
-public class TrayControl : MiniGame
+public class TrayControl : MonoBehaviour
 {
     GameObject pointMan;
     Rigidbody rb;
@@ -12,8 +12,12 @@ public class TrayControl : MiniGame
     //Vector3 handRight;
 
     bool hasGrabbed = false;
+    bool moveable = false;
 
- 
+    public float rotation = 0;
+
+
+    /*
     public override void OnGameFailed()
     {
         throw new System.NotImplementedException();
@@ -37,7 +41,7 @@ public class TrayControl : MiniGame
         throw new System.NotImplementedException();
     }
 
-
+    */
 
     // Start is called before the first frame update
     void Start()
@@ -49,13 +53,14 @@ public class TrayControl : MiniGame
     // Update is called once per frame
     void Update()
     {
-        if (hasGrabbed)
-        {
-            //berechne deltaY
-        }
+        if () { }
+        transform.Rotate(0, 0, rotation * Time.deltaTime);
         
-
     }
 
+    public void AdjustRotation(float newRotation)
+    {
+        rotation = newRotation;    
+    }
 
 }
